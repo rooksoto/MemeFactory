4542f0ce4bbae50ca484b4e097e038df5c2b2916
Rook Soto <rooksoto@gmail.com>
2017-01-09 13:56:42 -0500

    Layout Fixes

--- app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MainActivity.java
+++ app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MainActivity.java
@@ -1,9 +1,10 @@
-package rooksoto.c4q.nyc.memefactory;
+package rooksoto.c4q.nyc.memefactory.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import rooksoto.c4q.nyc.memefactory.Presenter.FileMaker;
+import rooksoto.c4q.nyc.memefactory.R;
 
 public class MainActivity extends AppCompatActivity {
 
