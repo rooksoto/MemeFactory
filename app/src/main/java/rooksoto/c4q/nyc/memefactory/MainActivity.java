b8138a157fbea95ae33bc913e9c219a1d888fe5f
Rook Soto <rooksoto@gmail.com>
2017-01-09 13:38:55 -0500

    Created FileMaker class, init. version

--- app/src/main/java/rooksoto/c4q/nyc/memefactory/MainActivity.java
+++ app/src/main/java/rooksoto/c4q/nyc/memefactory/MainActivity.java
@@ -3,11 +3,15 @@ package rooksoto.c4q.nyc.memefactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rooksoto.c4q.nyc.memefactory.Presenter.FileMaker;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FileMaker fileMaker = new FileMaker();
     }
 }
