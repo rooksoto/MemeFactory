303faa389e6088b2178491356c7563d93b3f2b46
nuily <mslinhui@gmail.com>
2017-01-18 02:44:06 -0500

    cleaned out dead code

--- app/src/main/java/rooksoto/c4q/nyc/memefactory/View/PhotoActivity.java
+++ app/src/main/java/rooksoto/c4q/nyc/memefactory/View/PhotoActivity.java
@@ -4,7 +4,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 import rooksoto.c4q.nyc.memefactory.Presenter.MemePagerAdapter;
 import rooksoto.c4q.nyc.memefactory.R;
@@ -13,7 +12,6 @@ import rooksoto.c4q.nyc.memefactory.R;
  * Created by ashiquechowdhury on 1/12/17.
  */
 public class PhotoActivity extends AppCompatActivity{
-    private ImageView memePhotoImageView;
     private MemePagerAdapter adapterViewPager;
 
     @Override
@@ -25,17 +23,5 @@ public class PhotoActivity extends AppCompatActivity{
         ViewPager vPager = (ViewPager) findViewById(R.id.vpager_photo);
         adapterViewPager = new MemePagerAdapter(getSupportFragmentManager(), memeImageUri);
         vPager.setAdapter(adapterViewPager);
-
-//        memePhotoImageView = (ImageView) findViewById(R.id.meme_photo_IView);
-
-//        Bitmap bitmap = null;
-//
-//        try {
-//            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), memeImageUri);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-//
     }
 }
