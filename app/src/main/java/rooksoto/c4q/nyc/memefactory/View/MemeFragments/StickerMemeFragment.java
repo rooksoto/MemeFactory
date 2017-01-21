2e253972eda4744ef678af4dcdb0c3e1b6273f8d
achowdhury3762 <achowdhury3762@gmail.com>
2017-01-21 15:12:00 -0500

    Stickers now inflate onClick

--- app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MemeFragments/StickerMemeFragment.java
+++ app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MemeFragments/StickerMemeFragment.java
@@ -3,6 +3,7 @@ package rooksoto.c4q.nyc.memefactory.View.MemeFragments;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,12 +20,15 @@ import rooksoto.c4q.nyc.memefactory.util.StickerImageView;
  * Created by ashiquechowdhury on 1/21/17.
  */
 
-public class StickerMemeFragment extends Fragment {
+public class StickerMemeFragment extends Fragment implements StickerAdapter.Listener{
     public static final String VAN_PAGE = "STICKER PAGE NUM";
     public static final String VAN_TITLE = "STICKER TITLE";
+    StickerAdapter.Listener mListener;
     ImageView memeImageView;
+    private FrameLayout canvas;
     private Uri uri;
     private RecyclerView stickerRecycler;
+    private StickerImageView iv_sticker;
 
     // newInstance constructor for creating fragment with arguments
     public static StickerMemeFragment newInstance(Uri uri, int page, String title) {
@@ -46,21 +50,39 @@ public class StickerMemeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stickers, container, false);
         memeImageView = (ImageView) view.findViewById(R.id.meme_sticker_imageview);
+        canvas = (FrameLayout) view.findViewById(R.id.my_sticker_frame);
+        stickerRecycler = (RecyclerView) view.findViewById(R.id.sticker_recycler);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Picasso.with(getContext()).load(uri).into(memeImageView);
 
+        loadMemeImage();
+        loadStickerRecycler();
 
-        FrameLayout canvas = (FrameLayout) view.findViewById(R.id.my_sticker_frame);
-
-        StickerImageView iv_sticker = new StickerImageView(getActivity());
-        iv_sticker.setImageDrawable(getResources().getDrawable(R.drawable.c10));
+        iv_sticker = new StickerImageView(getActivity());
+        iv_sticker.setImageDrawable(getResources().getDrawable(R.drawable.smiley_face));
         canvas.addView(iv_sticker);
-
     }
 
+    private void loadStickerRecycler() {
+        stickerRecycler.setLayoutManager(new LinearLayoutManager(
+                getActivity(),
+                LinearLayoutManager.HORIZONTAL,
+                false));
+        stickerRecycler.setAdapter(new StickerAdapter(this));
+    }
+
+    private void loadMemeImage() {
+        Picasso.with(getContext()).load(uri).into(memeImageView);
+    }
+
+    @Override
+    public void onStickerPressed(int stickerDrawable){
+        iv_sticker = new StickerImageView(getActivity());
+        iv_sticker.setImageDrawable(getResources().getDrawable(stickerDrawable));
+        canvas.addView(iv_sticker);
+    }
 }
