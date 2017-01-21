0a4a3d7c81fef716dfc0727a7bb180a26cf6755f
sderekj <dereksantos1989@gmail.com>
2017-01-21 11:01:45 -0500

    Restore Image on Orientation Change

--- app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MemeFragments/VanillaMemeFragment.java
+++ app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MemeFragments/VanillaMemeFragment.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -102,5 +103,20 @@ public class VanillaMemeFragment extends Fragment implements View.OnTouchListene
                 return false;
         }
         return true;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("memeImageUri", uri);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            Uri memeImageUri = (Uri) savedInstanceState.get("memeImageUri");
+            Picasso.with(getContext()).load(memeImageUri).into(memeImageView);
+        }
     }
 }
