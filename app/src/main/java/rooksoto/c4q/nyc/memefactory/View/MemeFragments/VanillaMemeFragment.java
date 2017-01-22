9dd794a970360192f7e6d84cb2f3de18643af34e
sderekj <dereksantos1989@gmail.com>
2017-01-22 13:31:31 -0500

    Removed dead code

--- app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MemeFragments/VanillaMemeFragment.java
+++ app/src/main/java/rooksoto/c4q/nyc/memefactory/View/MemeFragments/VanillaMemeFragment.java
@@ -155,20 +155,14 @@ public class VanillaMemeFragment extends Fragment implements View.OnTouchListene
             view.setDrawingCacheEnabled(true);
 
             view.buildDrawingCache();
-            //Define a bitmap with the same size as the view
             Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
-            //Bind a canvas to it
             Canvas canvas = new Canvas(returnedBitmap);
-            //Get the view's background
             Drawable bgDrawable = view.getBackground();
             if (bgDrawable != null) {
-                //has background drawable, then draw it on the canvas
                 bgDrawable.draw(canvas);
             } else {
-                //does not have background drawable, then draw white background on the canvas
                 canvas.drawColor(Color.parseColor("#FF757575"));
             }
-            // draw the view on the canvas
             view.draw(canvas);
             //return the bitmap
             return returnedBitmap;
